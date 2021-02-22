4d6c167ed1c6d13a75e0d3fd49fde32d360aff15
Prova <tabassumprova326@gmail.com>
2021-02-22 18:19:35 +0600

    Python Game

--- guess.py
+++ guess.py
@@ -1,2 +1,17 @@
 import random
-true_number=random.radint(1, 49)
+true_number = random.randint(1, 49)
+print(true_number)
+guess_number = int(input("Enter your guess between 1 and 49: "))
+print(guess_number)
+while True:
+    if guess_number == true_number:
+        print("YOU ARE RIGHT! GOOD JOB!")
+        break
+
+    elif guess_number < true_number:
+        print("YOUR GUESS IS LOW, TRY AGAIN")
+        guess_number = int(input("Enter your guess between 1 and 49: "))
+
+    elif guess_number > true_number:
+        print("YOUR GUESS IS HIGH, TRY AGAIN")
+        guess_number = int(input("Enter your guess between 1 and 49: "))
